2c524dc2baaf8cbafadf28c6955704f68666b48a
shuyao <wuqihan2117123@outlook.com>
2022-03-07 18:34:30 +0800

    fifth commit

---
+++ 爬虫/scrapy框架/middlePro/middlePro/middlewares.py
@@ -0,0 +1,65 @@
+# Define here the models for your spider middleware
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+
+from scrapy import signals
+import random
+# useful for handling different item types with a single interface
+from itemadapter import is_item, ItemAdapter
+
+class MiddleproDownloaderMiddleware:
+    # Not all methods need to be defined. If a method is not defined,
+    # scrapy acts as if the downloader middleware does not modify the
+    # passed objects.
+    USER_AGENT_LIST = [
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
+        "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",
+        "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"]
+    PROXY_http=[
+        '153.180.102.104:80',
+        '195.108.131.189:56055',
+    ]
+    PROXY_https=[
+        '120.83.49.90:9000',
+        '95.189.112.214:35508',
+    ]
+    #拦截请求
+    def process_request(self, request, spider):
+        #UA伪装
+        request.headers['User-Agent']=random.choice(self.USER_AGENT_LIST)
+        #验证代理ip是否生效
+        request.meta['proxy'] = '115.218.3.116:9000'
+        return None
+    #拦截所有的响应
+    def process_response(self, request, response, spider):
+        # Called with the response returned from the downloader.
+
+        # Must either;
+        # - return a Response object
+        # - return a Request object
+        # - or raise IgnoreRequest
+        return response
+    #拦截发生异常的请求
+    def process_exception(self, request, exception, spider):
+        if request.url.split(':')[0]=='http':
+           request.meta['proxy']='http://'+random.choice(self.PROXY_http)
+        else:
+            request.meta['proxy']='https://'+random.choice(self.PROXY_https)
+        return request  #将修正之后的对象进行重新的请求发送
+
---
+++ 爬虫/scrapy框架/middlePro/middlePro/spiders/middle.py
@@ -0,0 +1,13 @@
+import scrapy
+
+
+class MiddleSpider(scrapy.Spider):
+    #爬取百度
+    name = 'middle'
+    #allowed_domains = ['www.xxx.com']
+    start_urls = ['http://www.baidu.com/s?wd=ip']
+
+    def parse(self, response):
+        page_text=response.text
+        with open('ip.html','w',encoding='utf-8') as fp:
+            fp.write(page_text)
